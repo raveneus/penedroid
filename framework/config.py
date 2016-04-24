c4c11549a6bd3fc37161da2d2e8fe0f606ed212f
raveneus <raveneus.cwillguy@gmail.com>
2016-04-24 16:38:58 -0500

    Update config.py

--- framework/config.py
+++ framework/config.py
@@ -1,7 +1,28 @@
-def getToken(line):
-    token = ""
-    for letter in line:
-        if letter != " ":
-            token += letter
+import ConfigParser
+import imp
+global cwd
+import token.py
+f = open("cwd.txt", "r")
+cwd = f.read()
+f.close()
+global y = {}
+parser = ConfigParser.ConfigParser()
+def getConfig(configFile):
+    parser.read(configFile)
+    att = eval(parser.get("prf", "att"))
+    util = eval(parser.get("prf", "util"))
+    return (att, util)
+def load(att, util):
+    for module in att:
+        m = imp.load_source(module, cwd + "/modules/" + module)
+        if y[module]:
+            print "[-] Two modules with the same name."
         else:
-            break
+            y[module] = m
+    for module in util:
+        m = imp.load_source(module, cwd + "/modules/" + module)
+        if y[module]:
+            print "[-] Two modules with the same name."
+        else:
+            y[module] = m
+
