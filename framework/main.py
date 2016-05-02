1453a9c864c22038354f8ca99f68c51999d5c59a
raveneus <raveneus.cwillguy@gmail.com>
2016-05-02 17:17:49 -0500

    Update main.py

--- framework/main.py
+++ framework/main.py
@@ -1,3 +1,4 @@
+#import statements
 import os
 import ftplib
 import cmd
@@ -7,31 +8,56 @@ global y
 global cwd
 global attutilL
 global menus
-
+#define a token separator with colon: getTokenColon("foo:bar") => "foo"
+def getTokenColon(line):
+    token = ""
+    for letter in line:
+        if letter != ":":
+            token += letter
+        else:
+            break
+    return token
+#get current working directory
 cwd = config.cwd
+#get the array
 attutilL = config.getConfig(cwd + "/config/config.conf")
-config.load(attutilL[0].keys(), attutilL[1].keys())
+#load/import modules
+config.load(attutilL[0], attutilL[1])
+#set y (global array with module objects)
 y = config.y
 
+#define a function to load functions into menus
 def loadFunc(attutil):
+    utilmenu = utilMenu()
     for util in attutil[1].keys():
-        utilmenu = utilMenu()
         setattr(utilmenu, "do_" + util, y[util].main)
         def tmp(self):
             print "Usage: " + util
             print "%s: %s" % (util, attutil[1][util])
         setattr(utilmenu, "help_" + util, tmp)
-    for att in attutil[0].keys():
-        attmenu = attMenu()
-        setattr(attmenu, "do_" + att, y[att].main)
-        def temp(self):
-            print "Usage: %s" % att
-            print "%s: %s" % (att, attutil[0][att])
-        setattr(attmenu, "help_" + att, temp)
-    return [attmenu, utilmenu]
-
+        nixmenu = linuxMenu()
+        winmenu = windowsMenu()
+    for module, info in attutil[0].items():
+        name = getTokenColon(info)
+        #tests to see what os is it and load into appropriate menu
+        if getTokenColon(info[len(getTokenColon(info)) + 1:]) == "linux":
+            setattr(nixmenu, "do_" + name, y[att].main)
+            def temp(self):
+                print "Usage: %s" % name
+                #the next line takes the third thing in info (description) : "name:os:desc" for attack modules
+                print "%s: %s" % (name, info[len(getTokenColon(info[len(getTokenColon(info)) + 1:])) + 1:])
+            setattr(nixmenu, "help_" + name, temp)
+        if getTokenColon(info[len(getTokenColon(info)) + 1:]) == "windows":
+            setattr(winmenu, "do_" + name, y[att].main)
+            def temp(self):
+                print "Usage: %s" % name
+                #the next line takes the third thing in info (description) : "name:os:desc" for attack modules
+                print "%s: %s" % (name, info[len(getTokenColon(info[len(getTokenColon(info)) + 1:])) + 1:])
+            setattr(winmenu, "help_" + name, temp)
+    return [nixmenu, winmenu, utilmenu]
+#load the functions
 menus = loadFunc(attutilL)
-
+#define menus
 class menu(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
@@ -101,6 +127,14 @@ class utilMenu(cmd.Cmd):
     def help_spawn(self):
         print "Usage: spawn"
         print "spawn: spawn a shell"
+    def do_exit(self, args):
+        if args:
+            print "*** Number of arguments: needed 0"
+            return
+        return True
+    def help_exit(self):
+        print "Usage: exit"
+        print "exit: exit the utility context"
 class attMenu(cmd.Cmd):
     def __init__(self):
         cmd.Cmd.__init__(self)
@@ -108,3 +142,45 @@ class attMenu(cmd.Cmd):
         print "Usage: help [cmd]"
         print "cmd    the command to get help on"
         print "help: show help on a command or list commands"
+    def do_os(self, args):
+        if args == "linux":
+            menus[0].cmdloop("pdf-console:attack(linux)% ")
+        elif args == "windows":
+            menus[1].cmdloop("pdf-console:attack(windows)% ")
+        else:
+            print "*** Unknown argument: %s" % args
+            return
+    def help_os(self):
+        print "Usage: os [os]"
+        print "os    os to set (linux/windows) (sorry... no macs :) )"
+        print "os: set the os of the target"
+    def do_exit(self, args):
+        if args:
+            print "*** Number of arguments: needed 0"
+            return
+        return True
+    def help_exit(self):
+        print "Usage: exit"
+        print "exit: exit the attack context"
+class linuxMenu(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+    def do_exit(self, args):
+        if args:
+            print "*** Number of arguments: needed 0"
+            return
+        return True
+    def help_exit(self):
+        print "Usage: exit"
+        print "exit: exit the linux context"
+class windowsMenu(cmd.Cmd):
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+    def do_exit(self, args):
+        if args:
+            print "*** Number of arguments: needed 0"
+            return
+        return True
+    def help_exit(self):
+        print "Usage: exit"
+        print "exit: exit the windows context"
